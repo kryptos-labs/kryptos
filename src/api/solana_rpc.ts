56632e1d44f93abfb40a7c65b6dc2abe475f5aca
Karan <packirisamykaran@gmail.com>
2023-10-20 16:43:16 +0800

    spl token

--- src/api/solana_rpc.ts
+++ src/api/solana_rpc.ts
@@ -1,14 +1,18 @@
 // this is the main file for the solana rpc api
 
 import { Connection, clusterApiUrl, PublicKey } from "@solana/web3.js";
+import { getMint } from "@solana/spl-token";
 
 const connection = new Connection(clusterApiUrl("mainnet-beta"));
 
 export async function getTopHolders(publicKey: PublicKey) {
   try {
-    console.log("work");
-    const tokenAccounts = (await connection.getTokenLargestAccounts(publicKey))
-      .value;
+    const mintAddress = new PublicKey(
+      "SLNDpmoWva5oaBNrZfV8QksLZByp8c3bDjHqvnECmwE"
+    );
+
+    const tokenAccounts = await connection.getTokenSupply(mintAddress);
+
     console.log(tokenAccounts);
   } catch (e) {
     console.log(e);
--- src/services/whale/protocolWhales.ts
+++ src/services/whale/protocolWhales.ts
@@ -1,4 +1,5 @@
 import { ProtocolWhale, Symbol, TokenHeld } from "../../models/whaleModels";
+
 import { ProtocolInfo } from "../../constants/protocols";
 import {
   getAccountTokens,
@@ -8,7 +9,7 @@ import {
   AccountToken,
   TokenMeta,
 } from "../../api/solscan";
-import { PublicKey } from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import { getTopHolders } from "../../api/solana_rpc";
 import { handleError } from "../../utils/handleError";
 import { tokenTotalSupply } from "./metrics";
@@ -28,98 +29,111 @@ export async function getWhalesByProtocol(
     if (address === undefined) {
       handleError(new Error("Protocol address not found"));
     }
-    getTopHolders(new PublicKey(address));
 
-    // Get the protocol address
-
-    // Get the token metadata of the current Defi protocol
-    let tokenMetaData: TokenMeta = await getTokenMeta(address);
-
-    // Get the largest holders of the protocol token
-    let largestHolders: TokenHolders[] = await getLargestHolders(
-      address,
-      limit,
-      offset
+    getTopHolders(
+      new PublicKey("EsLAEeKA1dUJRiPpbTAm7r94KqGDAKe1ivH8PLAAuSxV")
     );
-
-    // (caching tokenMeta) list of tokenMeta for each token held by the whale mapped to symbol
-    let tokenMetaMap: Map<string, TokenMeta> = new Map();
-
-    for (let holder of largestHolders) {
-      let whale: ProtocolWhale;
-
-      let tokensHeld: AccountToken[] = await getAccountTokens(holder.owner);
-
-      let ownerTokens: TokenHeld[] = [];
-
-      let walletTotalValue = 0;
-
-      for (var token of tokensHeld) {
-        let tokenHeldMetaData: TokenMeta | undefined = undefined;
-
-        if (!tokenMetaMap.has(token.tokenSymbol)) {
-          tokenHeldMetaData = await getTokenMeta(token.tokenAddress);
-          tokenMetaMap.set(token.tokenSymbol, tokenHeldMetaData);
-        }
-
-        if (tokenHeldMetaData === undefined) {
-          continue;
-          throw new Error("Token metadata not found");
-        }
-
-        if (tokenHeldMetaData.price === undefined) {
-          // console.log(tokenHeldMetaData);
-          continue;
-          throw new Error("Token price not found");
-        }
-
-        if (token.tokenAmount.uiAmount === 0) {
-          continue;
-        }
-        let tokenPrice = tokenHeldMetaData.price;
-
-        let tokenValue = token.tokenAmount.uiAmount * tokenPrice;
-
-        walletTotalValue += tokenValue;
-
-        let ownerToken: TokenHeld = {
-          tokenAddress: token.tokenAddress,
-          symbol: token.tokenSymbol,
-          amount: token.tokenAmount.uiAmount,
-          account: token.tokenAccount,
-          value: tokenValue,
-          portfolioRatio: 0,
-        };
-        ownerTokens.push(ownerToken);
-      }
-
-      // Calculate the token ratio for each token held by the whale
-      for (let token of ownerTokens) {
-        token.portfolioRatio = (token.value / walletTotalValue) * 100;
-
-        // assign the token ration to ownerTokens
-        ownerTokens = ownerTokens.map((ownerToken) => {
-          if (ownerToken.symbol === token.symbol) {
-            ownerToken.portfolioRatio = token.portfolioRatio;
-          }
-          return ownerToken;
-        });
-      }
-
-      whale = {
-        protocolAddress: holder.address,
-        amount: Number(holder.amount) / Math.pow(10, holder.decimals),
-        owner: holder.owner,
-        tokensHeld: ownerTokens,
-        supplyRatio:
-          (Number(holder.amount) / Number(tokenMetaData.supply)) * 100,
-      };
-
-      whales.push(whale);
-    }
   } catch (error) {
-    handleError(error);
+    console.log(error);
   }
+
+  // try {
+  //   let address = ProtocolInfo[symbol].address;
+  //   if (address === undefined) {
+  //     handleError(new Error("Protocol address not found"));
+  //   }
+  //   getTopHolders(new PublicKey(address));
+
+  //   // Get the protocol address
+
+  //   // Get the token metadata of the current Defi protocol
+  //   let tokenMetaData: TokenMeta = await getTokenMeta(address);
+
+  //   // Get the largest holders of the protocol token
+  //   let largestHolders: TokenHolders[] = await getLargestHolders(
+  //     address,
+  //     limit,
+  //     offset
+  //   );
+
+  //   // (caching tokenMeta) list of tokenMeta for each token held by the whale mapped to symbol
+  //   let tokenMetaMap: Map<string, TokenMeta> = new Map();
+
+  //   for (let holder of largestHolders) {
+  //     let whale: ProtocolWhale;
+
+  //     let tokensHeld: AccountToken[] = await getAccountTokens(holder.owner);
+
+  //     let ownerTokens: TokenHeld[] = [];
+
+  //     let walletTotalValue = 0;
+
+  //     for (var token of tokensHeld) {
+  //       let tokenHeldMetaData: TokenMeta | undefined = undefined;
+
+  //       if (!tokenMetaMap.has(token.tokenSymbol)) {
+  //         tokenHeldMetaData = await getTokenMeta(token.tokenAddress);
+  //         tokenMetaMap.set(token.tokenSymbol, tokenHeldMetaData);
+  //       }
+
+  //       if (tokenHeldMetaData === undefined) {
+  //         continue;
+  //         throw new Error("Token metadata not found");
+  //       }
+
+  //       if (tokenHeldMetaData.price === undefined) {
+  //         // console.log(tokenHeldMetaData);
+  //         continue;
+  //         throw new Error("Token price not found");
+  //       }
+
+  //       if (token.tokenAmount.uiAmount === 0) {
+  //         continue;
+  //       }
+  //       let tokenPrice = tokenHeldMetaData.price;
+
+  //       let tokenValue = token.tokenAmount.uiAmount * tokenPrice;
+
+  //       walletTotalValue += tokenValue;
+
+  //       let ownerToken: TokenHeld = {
+  //         tokenAddress: token.tokenAddress,
+  //         symbol: token.tokenSymbol,
+  //         amount: token.tokenAmount.uiAmount,
+  //         account: token.tokenAccount,
+  //         value: tokenValue,
+  //         portfolioRatio: 0,
+  //       };
+  //       ownerTokens.push(ownerToken);
+  //     }
+
+  //     // Calculate the token ratio for each token held by the whale
+  //     for (let token of ownerTokens) {
+  //       token.portfolioRatio = (token.value / walletTotalValue) * 100;
+
+  //       // assign the token ration to ownerTokens
+  //       ownerTokens = ownerTokens.map((ownerToken) => {
+  //         if (ownerToken.symbol === token.symbol) {
+  //           ownerToken.portfolioRatio = token.portfolioRatio;
+  //         }
+  //         return ownerToken;
+  //       });
+  //     }
+
+  //     whale = {
+  //       protocolAddress: holder.address,
+  //       amount: Number(holder.amount) / Math.pow(10, holder.decimals),
+  //       owner: holder.owner,
+  //       tokensHeld: ownerTokens,
+  //       supplyRatio:
+  //         (Number(holder.amount) / Number(tokenMetaData.supply)) * 100,
+  //     };
+
+  //     whales.push(whale);
+  //   }
+  // } catch (error) {
+  //   handleError(error);
+  // }
 
   return whales;
 }
